b8d3d9681b25eef7ea34da5a149bdceb9eb047e6
Nihal Boina <nihal.boina@gmail.com>
2022-10-16 07:58:26 -0700

    pretty

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import './App.css';
-import Navbar from './components/navbar';
-import Home from './pages/home';
-import PostQuestion from './pages/postQuestion';
-import Profile from './pages/profile';
+import "./App.css";
+import Navbar from "./components/navbar";
+import Home from "./pages/home";
+import PostQuestion from "./pages/postQuestion";
+import Profile from "./pages/profile";
 import { Route, Routes } from "react-router-dom";
-import EnlargedPost from './pages/enlargedPost';
-import Login from './pages/login';
-
+import EnlargedPost from "./pages/enlargedPost";
+import Login from "./pages/login";
+import Landing from "./pages/landing";
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <div>
         <Routes>
-          <Route path="/" element={<Login/>}></Route>
-          <Route path="/home" element={<Home/>}></Route>
-          <Route path="/post-question" element={<PostQuestion/>}></Route>
-          <Route path="/view-profile" element={<Profile/>}></Route>
-          <Route path="/login" element={<Login/>}></Route>
-          <Route path="/post/:postID" element={<EnlargedPost/>}></Route>
+          <Route path="/" element={<Landing />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/post-question" element={<PostQuestion />}></Route>
+          <Route path="/view-profile" element={<Profile />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/post/:postID" element={<EnlargedPost />}></Route>
         </Routes>
       </div>
     </div>
